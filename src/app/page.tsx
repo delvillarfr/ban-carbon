98d3850a4bcf92f86928e6a80b2b0388833fa32e
Paco <delvillarfr@gmail.com>
2025-08-14 15:33:43 -0600

    Copy svg files from old/assets to public and port html to jsx in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,48 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+    <>
+      <header>
+        <img src="logo.svg" alt="Ultra Civic" className="logo" />
+      </header>
+      
+      <main>
+        <section className="hero-line">
+            <img src="hero-1.svg" alt="" className="hero-1" />
+            <img src="hero-2.svg" alt="" className="hero-2" />
+            <img src="hero-3.svg" alt="" className="hero-3" />
+            <div className="input-wrapper">
+                {/*}
+                  <input type="number" id="pollution-rights" min="1" max="99" value="1" className="rights-input" />
+                */}
+                <span className="custom-cursor">|</span>
+            </div>
+            <img src="hero-4.svg" alt="" className="hero-4" />
+        </section>
+        
+        <section className="hero-details">
+            <div className="pay-section">
+                <img src="pay-1.svg" alt="" className="pay-1" />
+                <span id="usd-price" className="price-value">24</span>
+                <img src="pay-2.svg" alt="" className="pay-2" />
+            </div>
+            
+            <div className="earn-section">
+                <img src="earn-1.svg" alt="" className="earn-1" />
+                <span id="pr-tokens" className="token-value">1</span>
+                <img src="earn-2.svg" alt="" className="earn-2" />
+            </div>
+        </section>
+        
+        <section className="action">
+            <a href="https://buy.stripe.com/4gMeVe5U9eZS5KS5349bO02" target="_blank" className="doit-button">
+                <img src="doit.svg" alt="Do It" />
+            </a>
+        </section>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+    
+      <footer>
+          <img src="contact.svg" alt="Contact" className="contact-info" />
       </footer>
-    </div>
+    </>
   );
 }
