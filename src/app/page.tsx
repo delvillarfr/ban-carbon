3d574d8a13e44ddb908e012b36be0da648d39f2a
Paco <delvillarfr@gmail.com>
2025-08-15 11:31:16 -0600

    Use images as Next.js <Image> components.
    Images were being added via <img>. <Image> provides various image
    optimizations.
    https://nextjs.org/learn/dashboard-app/optimizing-fonts-images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,114 @@
+import Image from 'next/image';
+
 export default function Home() {
   return (
     <>
       <header>
-        <img src="/logo.svg" alt="Ultra Civic" className="logo" />
+        <Image
+          src="/logo.svg"
+          alt="Ultra Civic"
+          className="logo"
+          width={1750}
+          height={401}
+        />
       </header>
       
       <main>
         <section className="hero-line">
-            <img src="/hero-1.svg" alt="" className="hero-1" />
-            <img src="/hero-2.svg" alt="" className="hero-2" />
-            <img src="/hero-3.svg" alt="" className="hero-3" />
+            <Image
+              src="/hero-1.svg"
+              alt=""
+              className="hero-1"
+              width={275}
+              height={50}
+            />
+            <Image
+              src="/hero-2.svg"
+              alt=""
+              className="hero-2"
+              width={524}
+              height={50}
+            />
+            <Image
+              src="/hero-3.svg"
+              alt=""
+              className="hero-3"
+              width={160}
+              height={49}
+            />
             <div className="input-wrapper">
                 {/*}
                   <input type="number" id="pollution-rights" min="1" max="99" value="1" className="rights-input" />
                 */}
                 <span className="custom-cursor">|</span>
             </div>
-            <img src="/hero-4.svg" alt="" className="hero-4" />
+            <Image
+              src="/hero-4.svg"
+              alt=""
+              className="hero-4"
+              width={284}
+              height={57}
+            />
         </section>
         
         <section className="hero-details">
             <div className="pay-section">
-                <img src="/pay-1.svg" alt="" className="pay-1" />
+                <Image
+                  src="/pay-1.svg"
+                  alt=""
+                  className="pay-1"
+                  width={86}
+                  height={37}
+                />
                 <span id="usd-price" className="price-value">24</span>
-                <img src="/pay-2.svg" alt="" className="pay-2" />
+                <Image
+                  src="/pay-2.svg"
+                  alt=""
+                  className="pay-2"
+                  width={54}
+                  height={30}
+                />
             </div>
             
             <div className="earn-section">
-                <img src="/earn-1.svg" alt="" className="earn-1" />
+                <Image
+                  src="/earn-1.svg"
+                  alt=""
+                  className="earn-1"
+                  width={109}
+                  height={37}
+                />
                 <span id="pr-tokens" className="token-value">1</span>
-                <img src="/earn-2.svg" alt="" className="earn-2" />
+                <Image
+                  src="/earn-2.svg"
+                  alt=""
+                  className="earn-2"
+                  width={33}
+                  height={31}
+                />
             </div>
         </section>
         
         <section className="action">
             <a href="https://buy.stripe.com/4gMeVe5U9eZS5KS5349bO02" target="_blank" className="doit-button">
-                <img src="/doit.svg" alt="Do It" />
+                <Image
+                  src="/doit.svg"
+                  alt="Do It"
+                  width={404}
+                  height={217}
+                />
             </a>
         </section>
       </main>
     
       <footer>
-          <img src="/contact.svg" alt="Contact" className="contact-info" />
+          <Image
+            src="/contact.svg"
+            alt="Contact"
+            className="contact-info"
+            width={447}
+            height={29}
+          />
       </footer>
     </>
   );
