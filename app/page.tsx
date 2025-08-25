237752ca12ab0e155fd2ada9a63ccaff2320e5e0
Paco <delvillarfr@gmail.com>
2025-08-25 12:44:17 -0600

    Create body and CSS skeletons.
    CSS is minimal. HTML follows best practices outlined in https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements

--- app/page.tsx
+++ app/page.tsx
@@ -1,78 +1,37 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <>
       <header>
-        <Image
-          src="/logo.svg"
-          alt="Ultra Civic"
-          className="logo"
-          width={1750}
-          height={401}
-        />
+        <nav>
+          <Link href="/">
+            <Image
+              src="/logo.svg"
+              alt="Ultra Civic"
+              className="logo"
+              width={1750}
+              height={401}
+            />
+          </Link>
+        </nav>
       </header>
-      
-      <main>
-        <section className="hero-container">
-          <div>
-            <h1 className="hero-line-1">
-              Polluters need legal permits to emit&nbsp;CO<sub>2</sub>.
-            </h1>
-            <h1 className="hero-line-2">
-              Retire them now.
-            </h1>
-          </div>
-        </section>
 
-        <section className="explain-container">
-        <div>
-          <Image
-            src="/explain-1.svg"
-            alt="Do It"
-            width={2030}
-            height={2030}
-          />
-        </div>
-        <div>
-          <Image
-            src="/explain-2.svg"
-            alt="Do It"
-            width={2030}
-            height={2030}
-          />
-        </div>
+      <main>
+        <section>
+          <h1>
+            Burn the legal permits that polluters need to emit CO2.
+          </h1>
         </section>
-        
-        <section className="action">
-            <a
-              href="https://buy.stripe.com/4gMeVe5U9eZS5KS5349bO02"
-              target="_blank"
-              className="doit-button"
-            >
-              <Image
-                src="/doit.svg"
-                alt="Do It"
-                width={404}
-                height={217}
-              />
-            </a>
+        <section>
+          <h2>
+            They're U.S. power plants who use coal, oil, or gas.
+          </h2>
         </section>
       </main>
-    
+
       <footer>
-        <p>
-          <a href="https://calendly.com/paco-ultracivic/30min" target="_blank">
-            Click here
-          </a>
-          &ensp;to schedule a meeting.
-        </p>
-        <p>
-          Chat with us:&ensp;
-          <a href="mailto:paco@ultracivic.com">
-            paco@ultracivic.com
-          </a>
-        </p>
       </footer>
     </>
   );
