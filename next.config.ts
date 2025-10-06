29a867492bf6b004191324cb44ee48049c5ea63d
Paco <delvillarfr@gmail.com>
2025-10-06 12:43:43 -0600

    Install and configure SVGR.
    SVGR uses webpack, not turbopack. I disabled turbopack.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,33 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    // Find existing SVG rule
+    const fileLoaderRule = config.module.rules.find((rule: any) =>
+      rule.test?.test?.('.svg')
+    )
+
+    config.module.rules.push(
+      // Reapply existing rule for SVG URLs
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      },
+      // Convert other SVGs to React components
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+        use: ['@svgr/webpack'],
+      }
+    )
+
+    // Exclude SVGs from file loader
+    fileLoaderRule.exclude = /\.svg$/i
+
+    return config
+  },
 };
 
 export default nextConfig;
