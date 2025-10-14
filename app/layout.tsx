0f5207abcdb7fd3f496bf6d7097690907673a80e
Paco <delvillarfr@gmail.com>
2025-10-14 17:34:45 -0600

    Add Vercel Web Analytics.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { inter } from '@/app/ui/fonts';
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "Ban carbon emissions",
