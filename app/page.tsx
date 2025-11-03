bbb80aabb5bb5367f9334d84600848e2c9de81dc
Paco <delvillarfr@gmail.com>
2025-11-03 10:21:43 -0600

    Replace chimney video with its high-res version.
    The new version has 13% higher video bitrate (837 kbps vs 740 kbps).

--- app/page.tsx
+++ app/page.tsx
@@ -27,9 +27,9 @@ export default function Home() {
               playsInline
               loop
               preload="metadata"
-              poster="/high-res_poster.jpg"
+              poster="/chimney_poster.jpg"
             >
-              <source src="/high-res-bt709.mp4" type="video/mp4" />
+              <source src="/chimney-bt709.mp4" type="video/mp4" />
             </video>
           </div>
         </div>
